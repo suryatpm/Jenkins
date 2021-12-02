5d633eb2d8070dde4fe2b1bd164ef737d3a9fdcb
suryatpm <95062384+suryatpm@users.noreply.github.com>
2021-12-02 21:39:25 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,13 @@ public class Helloworld{
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
-    System.out.println("Hello Agni");
+    System.out.println("Hello suryaa");
+      System.out.println("Hello surya");
+    System.out.println("Hello s");
+    System.out.println("Hello u");
+    System.out.println("Hello r");
+    System.out.println("Hello y");
+
 
  }
 
